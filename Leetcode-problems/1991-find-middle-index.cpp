ed52a3257d697fc8a00c70b6b69a5bdec75a6555
Biswajit Baliarsingh <biswajitbaliarsingh777@gmail.com>
2025-12-17 22:53:46 +0530

    1991

--- Leetcode-problems/1991-find-middle-index.cpp
+++ Leetcode-problems/1991-find-middle-index.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 class Solution
 {
 public:
@@ -26,3 +27,11 @@ public:
         return -1;
     }
 };
+/*
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.*/
